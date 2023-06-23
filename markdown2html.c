c7c894f1a4254b2f10b61322fe470277fbdc1760
Austin Slater <austinslater58@gmail.com>
2023-06-23 10:22:53 -0500

    fixed while loop, was not iterating properly accounted for items that were not being wrapped in semantic html tag now == <p>

--- markdown2html.c
+++ markdown2html.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 256
-
 #define MAX_OUTPUT_LENGTH 4096
 
 // Function to convert a Markdown heading to HTML
@@ -23,7 +22,7 @@ void convertListItem(const char *line, char *output)
 	snprintf(output, MAX_LINE_LENGTH, "<li>%s</li>\n", line + 2);
 }
 
-// Function to conver a .md link to html
+// Function to convert a .md link to HTML
 void convertLink(const char *line, char *output)
 {
 	char *textStart = strchr(line, '[');
@@ -32,7 +31,7 @@ void convertLink(const char *line, char *output)
 	char *urlEnd = strchr(line, ')');
 	*textEnd = '\0';
 	*urlEnd = '\0';
-	snprintf(output, MAX_LINE_LENGTH, "<a href=\"%s\">%s</a>", urlStart + 1, textStart + 1);
+	snprintf(output, MAX_LINE_LENGTH, "<a href=\"%s\">%s</a>\n", urlStart + 1, textStart + 1);
 }
 
 // Function to convert a .md file to HTML
@@ -49,32 +48,76 @@ void convertMarkdownToHTML(const char *inputFilename, const char *outputFilename
 	if (!outputFile)
 	{
 		printf("Error: Unable to open output file '%s'\n", outputFilename);
+		fclose(inputFile); // Close the input file
 		return;
 	}
 
 	char line[MAX_LINE_LENGTH];
-	char output[MAX_OUTPUT_LENGTH] = "";
+	char output[MAX_OUTPUT_LENGTH];
+
+	// Flag to indicate if the previous line was a heading or list item
+	int isPreviousLineHeading = 0;
+	int isPreviousLineListItem = 0;
 
 	while (fgets(line, MAX_LINE_LENGTH, inputFile) != NULL)
 	{
 		if (line[0] == '#')
 		{
+			if (isPreviousLineListItem)
+			{
+				fprintf(outputFile, "</li>\n");
+				isPreviousLineListItem = 0;
+			}
+
 			convertHeading(line, output);
+			fprintf(outputFile, "%s", output);
+			isPreviousLineHeading = 1;
 		}
 		else if (line[0] == '-')
 		{
+			if (isPreviousLineHeading)
+			{
+				fprintf(outputFile, "</h%d>\n", isPreviousLineHeading);
+				isPreviousLineHeading = 0;
+			}
+
 			convertListItem(line, output);
+			fprintf(outputFile, "%s", output);
+			isPreviousLineListItem = 1;
 		}
 		else if (line[0] == '[')
 		{
+			if (isPreviousLineHeading)
+			{
+				fprintf(outputFile, "</h%d>\n", isPreviousLineHeading);
+				isPreviousLineHeading = 0;
+			}
+
 			convertLink(line, output);
+			fprintf(outputFile, "%s", output);
 		}
 		else
 		{
-			fprintf(outputFile, "%s", output);
-		}
+			if (isPreviousLineListItem)
+			{
+				fprintf(outputFile, "</li>\n");
+				isPreviousLineListItem = 0;
+			}
 
-		fprintf(outputFile, "%s", output);
+			if (line[0] != '\n') // Skip empty lines
+			{
+				fprintf(outputFile, "<p>%s</p>\n", line); // Wrap regular text in <p> tags
+			}
+		}
+	}
+
+	if (isPreviousLineHeading)
+	{
+		fprintf(outputFile, "</h%d>\n", isPreviousLineHeading);
+	}
+	else if (isPreviousLineListItem)
+	{
+		fprintf(outputFile, "</li>\n");
 	}
 
 	fclose(inputFile);
